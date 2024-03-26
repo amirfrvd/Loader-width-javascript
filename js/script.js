1f149c1657872ad68e9a9b250143835cc974db0b
amir mohammad firoozvand <firoozvand655@gmail.com>
2024-03-26 18:18:11 +0330

    edit code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 const loaderElem = document.querySelector('.loader')
 
 window.addEventListener('load', function () {
-    //first way
-    // loaderElem.classList.add('hidden')
-
-    // second way
-    loaderElem.className += ' hidden' // class="loader hidden"
-
+    loaderElem.className += ' hidden'
 })
